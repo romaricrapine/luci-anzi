8ca829717b728d76a6c817a032eb1aca3f1a5eac
Lucifer <uberfdp@gmail.com>
2023-12-04 01:13:50 +0100

    [Add] New page added [Done] Page Show account style
    Signed-off-by: Lucifer <uberfdp@gmail.com>

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,9 +15,33 @@ class HomeController extends AbstractController
     }
 
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function test(): Response
+    public function dashboard(): Response
     {
         return $this->render('home/dasboard.html.twig');
+    }
+
+    #[Route('/account', name: 'app_show_account')]
+    public function showAccount(): Response
+    {
+        return $this->render('home/show_account.html.twig');
+    }
+
+    #[Route('/account/add', name: 'app_add_account')]
+    public function addAccount(): Response
+    {
+        return $this->render('home/add_account.html.twig');
+    }
+
+    #[Route('/order', name: 'app_show_order')]
+    public function showOrder(): Response
+    {
+        return $this->render('home/show_order.html.twig');
+    }
+
+    #[Route('/balance', name: 'app_balance')]
+    public function balance(): Response
+    {
+        return $this->render('home/balance.html.twig');
     }
 
     #[Route('/logout', name: 'app_logout')]
